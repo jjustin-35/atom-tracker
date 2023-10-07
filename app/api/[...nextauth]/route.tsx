2f5502c6797cfbcc6a8cf6dd617c76309f8f20f5
jjustin-35 <jjusttin11@gmail.com>
2023-10-07 17:03:16 +0800

    update: add next auth

---
+++ app/api/[...nextauth]/route.tsx
@@ -0,0 +1,24 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from 'next-auth/providers/facebook';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
+    }),
+  ],
+//   pages: {
+//     signIn: '/auth/login',
+//   },
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    publicRuntimeConfig: {
+        NODE_ENV: process.env.NODE_ENV,
+        ENV: process.env.ENV,
+    },
+    webpack: (config) => {
+        config.experiments = { ...config.experiments, topLevelAwait: true };
+
+    return config;
+    },
+}
 
 module.exports = nextConfig
