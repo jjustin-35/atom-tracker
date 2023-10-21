e54c52f486ca409e3a8452b5e177626a125a3f15
jjustin-35 <jjusttin11@gmail.com>
2023-10-22 02:16:28 +0800

    style: form api slice typo

--- redux/slices/form.tsx
+++ redux/slices/form.tsx
@@ -1,13 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import configs from '@/config';
 
-const signupApi = createApi({
+const signUpApi = createApi({
   reducerPath: 'signupApi',
   baseQuery: fetchBaseQuery({
     baseUrl: configs.HOST,
   }),
   endpoints: (builder) => ({
-    getAuthForm: builder.query({
+    postSignUp: builder.mutation({
       query: () => '/api/signup',
     }),
   }),
