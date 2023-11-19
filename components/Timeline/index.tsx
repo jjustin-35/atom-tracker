b76293e1e19a8355efd3bbc67d1f7e1364add05e
jjustin-35 <jjusttin11@gmail.com>
2023-11-20 01:25:27 +0800

    update: add timer to update timeline every hour

--- components/Timeline/index.tsx
+++ components/Timeline/index.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Timeline as TimelineWrapper } from '@mui/lab';
-import { TimeNodeType } from '@/constants/types/api';
-import { TimeNodeVariantType } from '@/constants/types/timenode';
-import { ModalStateType, openModal as openModalAction } from '@/redux/slices/modal';
+import dayjs, { Dayjs } from 'dayjs';
+
+import type { TimeNodeType } from '@/constants/types/api';
+import type { TimeNodeVariantType } from '@/constants/types/timenode';
+import {
+  ModalStateType,
+  openModal as openModalAction,
+} from '@/redux/slices/modal';
 import { EDIT_TIMNODE } from '@/constants/modalType';
 import Item from './Item';
 
@@ -15,16 +21,9 @@ type Props = {
 const Timeline = ({ data }: Props) => {
   const dispatch = useDispatch();
   const openModal = (data: ModalStateType) => dispatch(openModalAction(data));
-
-  const itemClickHandler = ( time: number, timenodeId?: TimeNodeType['id']) => {
-    openModal({
-      modalType: EDIT_TIMNODE,
-      modalProps: { timenodeId, time },
-    });
-  };
-
-  const date = new Date();
-  const currentHour = date.getHours();
+  const initTime = dayjs();
+  const [time, setTime] = useState<Dayjs>(initTime);
+  const currentHour = time.hour();
   const hours = Array.from({ length: currentHour + 1 }, (_, i) => i);
   const items = hours.map((hour) => {
     const item = data?.find((item) => item.time === hour);
@@ -38,6 +37,31 @@ const Timeline = ({ data }: Props) => {
       isNewItem: true,
     };
   });
+
+  // update time every hour
+  useEffect(() => {
+    const nextHour = currentHour + 1;
+    const delay = (() => {
+      if (nextHour < 24) {
+        const nextTime = dayjs().hour(nextHour);
+        return nextTime.diff(time, 'millisecond');
+      }
+      const nextTime = dayjs().add(1, 'day').hour(nextHour - 24);
+      return nextTime.diff(time, 'millisecond');
+    })();
+
+    const timer = setInterval(() => {
+      setTime(dayjs());
+    }, delay);
+    return () => clearInterval(timer);
+  }, [time]);
+
+  const itemClickHandler = (time: number, timenodeId?: TimeNodeType['id']) => {
+    openModal({
+      modalType: EDIT_TIMNODE,
+      modalProps: { timenodeId, time },
+    });
+  };
 
   return (
     <TimelineWrapper position="right">
